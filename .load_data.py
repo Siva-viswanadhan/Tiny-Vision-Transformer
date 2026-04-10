db78819a1c41a10375070f143965c4fdbe718ea7
siva-viswanadhan <sivaviswanadhan7@gmail.com>
2026-04-10 17:05:45 +0530

    done dataset

--- .load_data.py
+++ .load_data.py
@@ -3,12 +3,30 @@ import pandas as pd
 from torch.utils.data import Dataset
 import os
 import torchvision.transforms as T
+import torch
 
 class MnistDataset(Dataset):
     def __init__(self,image,label):
         self.image= image
         self.label=label
 
-        self.image_transform=T.compose(
-            T.Resize
-        )
+        
+
+        return image,label
+
+    def __len__(self,label):
+        return len(self.label)
+    
+    def __getitem__(self, index):
+
+        image=self.image[index]
+        label=self.label[index]
+
+        image= torch.tensor(image,dtype=torch.float32)
+        image=image/255.0
+        image=image.unsqueeze(0)  #---> add channel dimension
+
+        label=torch.tensor(label,dtype=torch.float32)
+
+
+        return image,label
