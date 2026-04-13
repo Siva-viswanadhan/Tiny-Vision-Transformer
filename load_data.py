592b44096b418bff3379fc9df2bafaf28ae2675e
siva-viswanadhan <sivaviswanadhan7@gmail.com>
2026-04-13 16:43:27 +0530

    added idx2numpy to requirements

--- load_data.py
+++ load_data.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset
 import os
 import torchvision.transforms as T
 import torch
+import idx2numpy
 
 class MnistDataset(Dataset):
     def __init__(self,images,labels):
@@ -30,3 +31,35 @@ class MnistDataset(Dataset):
 
 
         return image,label
+    
+
+
+train_images_path=r"C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\train-images.idx3-ubyte"
+train_labels_path=r"C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\train-labels.idx1-ubyte"
+
+test_images_path=r"C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\t10k-images.idx3-ubyte"
+test_labels_path=r"C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\t10k-labels.idx1-ubyte"
+
+
+
+
+x_train = idx2numpy.convert_from_file(train_images_path)
+x_train = x_train.reshape(60000,1,28,28)/255
+print(x_train.shape)
+x_train = torch.tensor(x_train,dtype=torch.float32).to(device)
+
+y_train = idx2numpy.convert_from_file(train_labels_path)
+y_train=y_train
+print(y_train.shape)
+
+y_train = torch.tensor(y_train,dtype=torch.long).to(device)
+
+x_test=idx2numpy.convert_from_file(test_images_path)
+x_test=x_test.reshape(10000,1,28,28)/255
+print(x_test.shape)
+
+x_test=torch.tensor(x_test,dtype=torch.float32).to(device)
+
+y_test=idx2numpy.convert_from_file(test_labels_path)
+
+y_test=torch.tensor(y_test,dtype=torch.long).to(device)
--- model.py
+++ model.py
@@ -3,12 +3,13 @@ import pandas
 import torch
 import os
 from load_data import MnistDataset
+from torch.utils.data import DataLoader
 
+train_dataset=MnistDataset(r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\train-images.idx3-ubyte',
+                           r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\train-labels.idx1-ubyte')
 
-loader=MnistDataset(r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\t10k-images.idx3-ubyte',
-                    r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\t10k-labels.idx1-ubyte',
-                    r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\train-images.idx3-ubyte',
-                    r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\train-labels.idx1-ubyte'
-)
+test_dataset=MnistDataset(r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\t10k-images.idx3-ubyte',
+                          r'C:\Users\siva-INC-5712\Desktop\Tiny Vision Transformer\Mnist\t10k-labels.idx1-ubyte')
 
-(x_train,y_train),(x_test,y_test)=loader.load_data()
+train_loader=DataLoader(train_dataset,batch_size=10,shuffle=True)
+test_loader=DataLoader(test_dataset,batch_size=10,shuffle=False)
